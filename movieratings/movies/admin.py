91b71eeb9d7df25ec76fddaf779b1060b6f892f0
Ryan Burton <rryanburton@gmail.com>
2015-10-07 07:57:59 -0400

    updated admin.py

--- movieratings/movies/admin.py
+++ movieratings/movies/admin.py
@@ -6,5 +6,14 @@ class RaterAdmin(admin.ModelAdmin):
     list_display = ['age', 'gender', 'occupation', 'zipcode']
 
 
+class Movie(models.Model):
+    list_display = ['movietitle']
+
+
+class Rating(models.Model):
+    list_display = ['userid', 'movieid', 'rating']
+
 # Register your models here.
 admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(Rating)
