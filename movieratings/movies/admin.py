c85587a359e56ec5d3f61f1daa41cf7620736edf
Ryan Burton <rryanburton@gmail.com>
2015-10-06 20:53:40 -0400

    Rater started

--- movieratings/movies/admin.py
+++ movieratings/movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Rater, Movie, Rating
+
+
+class RaterAdmin(admin.ModelAdmin):
+    list_display = ['age', 'gender', 'occupation', 'zipcode']
+
 
 # Register your models here.
+admin.site.register(Rater)
--- movieratings/movies/models.py
+++ movieratings/movies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Rater(models.Model):
+    userid = models.IntegerField()
+    age = models.IntegerField()
+    gender = models.CharField(max_length=1)
+    occupation = models.IntegerField()
+    zipcode = models.CharField(max_length=10)
+
+
+class Movie(models.Model):
+    movieid = models.IntegerField()
+    movietitle = models.CharField(max_length=100)
+
+
+class Rating(models.Model):
+    userid = models.ForeignKey(Rater)
+    movieid = models.ForeignKey(Movie)
+    rating = models.IntegerField()
