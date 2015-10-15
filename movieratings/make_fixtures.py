edacb60f91eda3b6222af1c5ca518ec6f0dab9ac
Ryan Burton <rryanburton@gmail.com>
2015-10-15 00:32:14 -0400

    converted to postgres

--- movieratings/make_fixtures.py
+++ movieratings/make_fixtures.py
@@ -1,13 +1,5 @@
 import csv
 import json
-from faker import Faker
-from django.conf import settings
-
-
-# from django.contrib.auth.models import User
-
-# settings.configure()
-
 
 raters_source = '../ml-1m/users.dat'
 ratings_source = '../ml-1m/ratings.dat'
@@ -19,13 +11,10 @@ movies_fixture = 'fixtures/movies.json'
 
 
 def main():
-    settings.configure()
-
     load_all_data()
 
 
 def load_all_data():
-
     print("\nloading {}".format(raters_source))
     load_raters_data()
     print("{} is ready!\n".format(raters_fixture))
@@ -40,24 +29,13 @@ def load_all_data():
 
 
 def load_raters_data():
-    from django.contrib.auth.models import User
-    User.objects.all().delete()
-    count = 1
-    fake = Faker()
     raters = []
     with open(raters_source) as f:
         reader = csv.DictReader(
             [line.replace('::', '\t') for line in f],
             fieldnames='UserID::Gender::Age::Occupation::Zip-code'.split('::'),
             delimiter='\t')
-        user_set = {fake.user_name() for _ in range(8000)}
-        user_list = [x for x in user_set]
         for row in reader:
-            # print('Reading row: {}'.format(count))
-            auth_user = User(username=user_list[count],
-                             email=fake.email(),
-                             password='password',
-                             pk=row['UserID'])
             rater = {
                 'fields': {
                     'gender': row['Gender'],
@@ -66,11 +44,9 @@ def load_raters_data():
                     'zipcode': row['Zip-code'],
                 },
                 'model': 'movieapp.Rater',
-                'pk': auth_user.pk,
+                'pk': int(row['UserID']),
             }
             raters.append(rater)
-            auth_user.save()
-            count += 1
 
     with open(raters_fixture, 'w') as f:
         f.write(json.dumps(raters))
--- movieratings/movieratings/settings.py
+++ movieratings/movieratings/settings.py
@@ -79,10 +79,24 @@ WSGI_APPLICATION = 'movieratings.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    'default':
+        {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'movieratings',
+            'USER': 'movieratings',
+            'PASSWORD': 'password',
+            'HOST': '',
+            'PORT': '',
+        }
+
+
+
+
+
 }
 
 
